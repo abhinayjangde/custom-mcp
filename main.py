46988d4f685116774db129ca2b58f4b80d22d4fc
Abhinay Jangde <abhinayjangde@gmail.com>
2026-03-07 09:23:53 +0530

    Refactor code structure for improved readability and maintainability

---
+++ main.py
@@ -0,0 +1,16 @@
+from fastmcp import FastMCP
+
+mcp = FastMCP(name="custome-mcp")
+
+@mcp.tool
+def add(a: float, b: float) -> float:
+    """Add two numbers."""
+    return a + b
+
+@mcp.tool
+def subtract(a: float, b: float) -> float:
+    """Subtract two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    mcp.run()
